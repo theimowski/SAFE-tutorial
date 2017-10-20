dad80f50e004f8c65a4fd64a8ab96efaf5cb4ddf
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-10-20 21:26:29 +0200

    install Client

--- build.fsx
+++ build.fsx
@@ -5,17 +5,50 @@ open System
 open Fake
 
 let serverPath = "./src/Server/" |> FullName
+let clientPath = "./src/Client" |> FullName
 
-let runDotnet workingDir args =
-    let result =
-        ExecProcess (fun info ->
-            info.FileName <- "dotnet"
-            info.WorkingDirectory <- workingDir
-            info.Arguments <- args) TimeSpan.MaxValue
-    if result <> 0 then failwithf "dotnet %s failed" args
+let platformTool tool winTool =
+  let tool = if isUnix then tool else winTool
+  tool
+  |> ProcessHelper.tryFindFileOnPath
+  |> function Some t -> t | _ -> failwithf "%s not found" tool
+
+let nodeTool = platformTool "node" "node.exe"
+let yarnTool = platformTool "yarn" "yarn.cmd"
+
+let mutable dotnetCli = "dotnet"
+
+let run cmd args workingDir =
+  let result =
+    ExecProcess (fun info ->
+      info.FileName <- cmd
+      info.WorkingDirectory <- workingDir
+      info.Arguments <- args) TimeSpan.MaxValue
+  if result <> 0 then failwithf "'%s %s' failed" cmd args
+
+Target "Clean" DoNothing
+
+Target "InstallDotNetCore" (fun _ ->
+  dotnetCli <- DotNetCli.InstallDotNetSDK "2.0.0"
+)
+
+Target "InstallClient" (fun _ ->
+  printfn "Node version:"
+  run nodeTool "--version" __SOURCE_DIRECTORY__
+  printfn "Yarn version:"
+  run yarnTool "--version" __SOURCE_DIRECTORY__
+  run yarnTool "install --frozen-lockfile" __SOURCE_DIRECTORY__
+)
 
 Target "Build" (fun () -> 
-  runDotnet serverPath "build"
+  run dotnetCli "build" serverPath
+  run dotnetCli "restore" clientPath
+  run dotnetCli "fable webpack -- -p" clientPath
 )
+
+"Clean"
+  ==> "InstallDotNetCore"
+  ==> "InstallClient"
+  ==> "Build"
 
 RunTargetOrDefault "Build"
