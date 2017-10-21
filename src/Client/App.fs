be4f4d023e8cd52d542c745f931a2af877b51001
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-10-21 11:22:37 +0200

    Hello world from Elmish with React

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -1,11 +1,35 @@
 module App
 
-open Fable.Import
+open Elmish
+open Elmish.React
 
-let GreetingWords = "Hello world!"
+open Fable.Helpers.React
 
-// Browser is in Fable.Import namespace.
-let content = Browser.document.getElementById "content"
-content.innerHTML <- (sprintf "<h1>%s</h1>" GreetingWords)
+type Model =
+  { X : int }
 
-Browser.window.console.log GreetingWords
+type Msg =
+| Increment
+| Decrement
+
+let init () =
+  { X = 0 }, Cmd.ofMsg Increment
+
+let update msg model =
+  match msg,model with
+  | Increment, { X = x } when x >= 100 ->
+    { model with X = x - 1 }, Cmd.ofMsg Decrement
+  | Increment, _ ->
+    { model with X = model.X + 1 }, Cmd.ofMsg Increment
+  | Decrement, { X = x } when x <= 0 ->
+    { model with X = x + 1 }, Cmd.ofMsg Increment
+  | Decrement, _ ->
+    { model with X = model.X - 1 }, Cmd.ofMsg Decrement
+
+let view model dispath =
+  div []
+   [ h1 [] [ str (sprintf "Counter: %d" model.X) ] ]
+
+Program.mkProgram init update view
+|> Program.withReact "elmish-app"
+|> Program.run
