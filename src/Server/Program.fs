42ea1229796ba60bb607698621f7c9d138bcb543
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-10-30 21:36:57 +0100

    /api/genres Server

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -3,13 +3,29 @@
 open System.Net
 
 open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
 
-let path = Path.Combine("src","Client") |> Path.GetFullPath
+let clientPath = Path.Combine("src","Client") |> Path.GetFullPath
 let port = 8085us
 
 let config =
   { defaultConfig with 
-      homeFolder = Some path
+      homeFolder = Some clientPath
       bindings = [ HttpBinding.create HTTP (IPAddress.Parse "0.0.0.0") port ] }
 
-startWebServer config Files.browseHome
+let getGenres = 
+  [ "Rock"; "Pop"; "Disco"; "Blues"]
+  |> List.map (sprintf "\"%s\"")
+  |> String.concat ","
+  |> sprintf "[%s]"
+  |> OK
+
+let app =
+  choose [
+    path "/api/genres" >=> getGenres
+    Files.browseHome
+  ]
+
+startWebServer config app
